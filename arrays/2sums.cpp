6352a9289137e2e621911591ce14f17290ffbf03
harshcoder <diamondgamer1111@gmail.com>
2025-02-16 23:34:55 +0530

    write something again

--- arrays/2sums.cpp
+++ arrays/2sums.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> funct(vector<int> &nums, int target)
+vector<int> twosum(vector<int> given_num, int target)
 {
-    // I am thinking of a unordered map approach will store the compliment in my map and and keep storing all the elements in my map
+    // i need to think of logic here before coding
+    // 1 2 3 4 5 and target is 7 and it is also given that we can assume the output of the giveninput will be only one
 
-    unordered_map<int, int> mpp;
-    for (int i = 0; i < nums.size(); i++)
+    // i can use a for loop n^2 mai hpjayga
+    // i can use a two pointer but that will  be nlogn approach which is not soo good
+
+    // i can use a hashmap in this case
+    // To make this Agent, we’re going to use smolagents, a library that provides a framework for developing your agents with ease. as
+
+    unordered_map<int, int> mpp; // here i will store the number as key and value index which will
+
+    // i can insert it int the process of checking the complement thoehg which we can find another number and we can return its index soo lets do that
+
+    for (int i = 0; i < given_num.size(); i++)
     {
-        int complement = target - nums[i];
+        int complement = target - given_num[i];
         if (mpp.find(complement) != mpp.end())
         {
             return {mpp[complement], i};
         }
-        mpp[nums[i]] = i;
+        mpp[given_num[i]] = i;
     }
     return {};
 }
 
 int main()
 {
-    vector<int> harsh = {2, 7, 11, 15};
+    vector<int> input = {1, 2, 3, 4, 5};
     int target = 9;
 
-    vector<int> result = funct(harsh, target);
-    for (int index : result)
-    {
-        cout << index << " ";
-    }
-    cout << endl;
+    vector<int> result = twosum(input, target);
+    cout << result[0] << " " << result[1] << endl;
 }
